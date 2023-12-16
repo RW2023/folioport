d0712c009b15891c999a541969f42572899820f1
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-16 05:38:17 -0500

    Update DarkModeToggle input value

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -9,7 +9,7 @@ const DarkModeToggle = () => {
   return (
     <label className="swap swap-rotate">
       {/* this hidden checkbox controls the state */}
-      <input type="checkbox" className="theme-controller" value="dark" />
+      <input type="checkbox" className="theme-controller" value="business" />
 
       {/* sun icon */}
       <svg
