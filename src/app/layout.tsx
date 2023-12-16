71dd72fb78acb695f484b00fc06e98313631e307
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-16 05:38:30 -0500

    Add Navbar and Footer components to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Navbar from '@/components/ui/Navbar'
+import Footer from '@/components/ui/Footer'
 
 
 
@@ -14,10 +16,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" data-theme='business'>
+    <html lang="en" data-theme="light">
       <body>
+        <Navbar />
         {children}
-        </body>
+      </body>
+      <Footer />
     </html>
-  )
+  );
 }
