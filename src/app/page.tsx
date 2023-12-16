150867f19e3f04384b0e0a34e3e132c2b2bbf739
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-15 19:10:24 -0500

    add paragraph font and style to test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@
 
 export default function Home() {
   return (
-    <div className='flex flex-col justify-center items-center text-5xl font-bold min-h-screen'>
-      <h1>Home Page</h1>
+    <div className="flex flex-col justify-center items-center  min-h-screen">
+      <h1 className="text-5xl font-bold m-1 p-1">Home Page</h1>
+      <p className="text-xl">This is paragraph font on the Home Page</p>
     </div>
-  )
+  );
 }
