fcb9f206b5f681f145b5af72dbec1617b333fb18
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-16 02:25:51 -0500

    add components from other project will customize each for this one

---
+++ src/app/about/page.tsx
@@ -0,0 +1,73 @@
+import Heading from '@/components/ui/Heading';
+import SubHeading from '@/components/ui/SubHeading';
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="bg-base-200 min-h-screen">
+      <div className="container mx-auto p-6">
+        <Heading title="About" iconClass="fas fa-info" />
+
+        <div className="card shadow-xl mb-4 glass bg-black">
+          <div className="glass card-body text-headline">
+            <SubHeading
+              title="The Journey of Capturing Moments"
+              iconClass="fas fa-camera"
+            />
+            <p>
+              This site is my digital canvas - a central spot on the internet
+              for showcasing my photography. It&apos;s born from a passion for
+              capturing the fleeting moments and the beauty of the world through
+              the lens.
+            </p>
+          </div>
+        </div>
+
+        <div className="card  shadow-xl mb-4 glass bg-black">
+          <div className="card-body text-headline">
+            <SubHeading
+              title="A Blend of Art and Technology"
+              iconClass="fas fa-paint-brush"
+            />
+            <p>
+              Beyond just photographs, this site represents a journey in digital
+              artistry, combining creative expression with technological
+              innovation. Each image tells a story, capturing emotions and
+              landscapes in unique compositions.
+            </p>
+          </div>
+        </div>
+
+        <div className="card shadow-xl mb-4 glass bg-black text-headline">
+          <div className="card-body">
+            <SubHeading
+              title="Coding Meets Creativity"
+              iconClass="fas fa-code"
+            />
+            <p>
+              Building this site has been a personal exploration in web
+              development and design, intertwining my coding practice with my
+              passion for photography, demonstrating how technology can enhance
+              artistic expression.
+            </p>
+          </div>
+        </div>
+
+        <div className="card bg-black shadow-xl mb-6 glass text-headline">
+          <div className="card-body">
+            <SubHeading
+              title="A Continuous Evolution"
+              iconClass="fas fa-cogs"
+            />
+            <p>
+              As my skills in both photography and coding grow, I look forward
+              to evolving this site further. It&apos;s more than just a gallery;
+              it&apos;s a growing portfolio reflecting my journey as a
+              photographer and web developer.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-center p-10 bg-base-300 text-base-content rounded mt-1">
+      <nav className="grid grid-flow-col gap-4">
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/feedback">Feedback</Link>
+      </nav>
+      <nav>
+        <div className="grid grid-flow-col gap-4 text-3xl">
+          {/* GitHub */}
+          <a
+            href="https://github.com/RW2023/rw-images"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub"
+          >
+            <i className="fab fa-github"></i>
+            <span className="sr-only">GitHub</span>
+          </a>
+          {/* Instagram */}
+          <a
+            href="https://www.instagram.com/ryanwilsonimages/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
+          >
+            <i className="fab fa-instagram"></i>
+            <span className="sr-only">Instagram</span>
+          </a>
+        </div>
+      </nav>
+      <aside>
+        <p>
+          Copyright © 2023 - All rights reserved by
+          <a
+            href="https://nextport-alpha.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span> Ryan Wilson</span>
+          </a>
+        </p>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
