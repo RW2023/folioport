bbf1cb94abe2e5fd27697fe12a8becc80a77fe69
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-16 18:58:27 -0500

    Update grid gap in About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,7 +12,7 @@ export default function About() {
           iconClass="fas fa-code"
         />
 
-        <div className="grid grid-col-1 gap-6 md:col-1 lg:grid-cols-2 lg-w-3/4">
+        <div className="grid grid-col-1 gap-3 md:col-1 lg:grid-cols-2 lg-w-3/4">
           <div className="card bg-base-300 shadow-xl mb-6 glass  mx-auto text-lg lg:w-3/4 ">
             <div className="card-body">
               <SubHeading title="My Story" iconClass="fas fa-book" />
