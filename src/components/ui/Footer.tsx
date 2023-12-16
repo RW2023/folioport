6ae946453bf01046fdbc5d95be7f21ceb81c6ce3
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-16 14:49:16 -0500

    remove margin that created a sliver peeking through

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-10 bg-base-300 text-base-content rounded mt-1">
+    <footer className="footer footer-center p-10 bg-base-300 text-base-content rounded">
       <nav className="grid grid-flow-col gap-4">
         <Link href="/about">About</Link>
         <Link href="/contact">Contact</Link>
