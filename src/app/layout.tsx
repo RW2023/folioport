6f95f633c1d4daadffe606d6ea365c289c3b88d5
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-16 11:18:44 -0500

    Add font-awesome stylesheet to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="light">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+        />
+      </head>
       <body>
         <Navbar />
         {children}
