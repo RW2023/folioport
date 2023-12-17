f6e1666e9547951798825f33e92153ba15a03f44
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-17 05:07:42 -0500

    Add ChakraProvider to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,15 @@ import '@/app/globals.css';
 import { AnimatePresence } from 'framer-motion';
 import type { AppProps } from 'next/app';
 import Layout from '@/app/layout';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <Layout>
       <AnimatePresence mode="wait">
-        <Component {...pageProps} key={router.route} />
+        <ChakraProvider>
+          <Component {...pageProps} key={router.route} />
+        </ChakraProvider>
       </AnimatePresence>
     </Layout>
   );
