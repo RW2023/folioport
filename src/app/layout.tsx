05bf65cd5b48e695215032fdfcdb83ff27d2b027
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-17 05:33:56 -0500

    Add title and meta description to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,8 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
         />
+        <title>Ryan Wilson Developer</title>
+        <meta name="description" content="Ryan Wilson's Developer Portfolio" />
       </head>
       <body>
         <Navbar />
