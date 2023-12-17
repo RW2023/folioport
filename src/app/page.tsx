7cfe47740fdd223c01bed6f0ee79d33d3c535249
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-17 04:05:18 -0500

    Update Home Page layout and add Heading and SubHeading components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Heading from "@/components/ui/Heading";
+import SubHeading from "@/components/ui/SubHeading";
 
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center  min-h-screen">
-      <h1 className="text-5xl font-bold m-1 p-1">Home Page</h1>
+    <div className="flex flex-col  items-center  min-h-screen m-4 p-1 bg-base-100">
+     <Heading title="home page" iconClass="fas fa-home" />
+      <SubHeading title="This is a subheading on the Home Page" iconClass="fas fa-info-circle" />
       <p className="text-xl">This is paragraph font on the Home Page</p>
     </div>
   );
