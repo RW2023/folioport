62a326edef1a2b0e5bbd15031943469347593be0
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-16 19:24:21 -0500

    Add framer-motion animation to page transitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 // pages/_app.js or app/pages/_app.js
 import '@/app/globals.css';
+import { AnimatePresence } from 'framer-motion';
 import type { AppProps } from 'next/app';
 import Layout from '@/app/layout';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
     </Layout>
   );
 }
