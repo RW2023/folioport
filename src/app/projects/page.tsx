cae5f698b0e67e64967946e45c9c06ae93114380
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-21 15:34:30 -0500

    Refactor project page layout and styles

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -11,7 +11,7 @@ const page: FC<Props> = (): JSX.Element => {
   return (
     <>
       <div className="flex flex-col items-center m-4 p-1 bg-base-100">
-        <Heading title="projects" iconClass="fas fa-cogs" />{' '}
+        <Heading title="projects" iconClass="fas fa-cogs" />
         <SubHeading
           title="This is a subheading on the projects Page"
           iconClass="fas fa-project-diagram"
@@ -19,8 +19,9 @@ const page: FC<Props> = (): JSX.Element => {
         <p className="text-xl">This is paragraph font on the Projects Page</p>
       </div>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 mb-5">
+        {/* First Card - Original Style */}
         <motion.div
-          className="card  items-center border rounded-md bg-base-300 mx-5 flex flex-col"
+          className="card items-center border rounded-md bg-base-300 mx-5 flex flex-col"
           initial={{ opacity: 0, x: -100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5 }}
@@ -47,95 +48,56 @@ const page: FC<Props> = (): JSX.Element => {
                 component to optimize the images for the web. I also used the
                 Framer Motion library to animate.
               </p>
-              <a href='https://rw-images.vercel.app/' target='_blank' rel='noopener noreferrer'>
-                <button type='button' aria-label='live version' className="btn btn-primary m-1">Live Version</button>
+              <a
+                href="https://rw-images.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button
+                  type="button"
+                  aria-label="live version"
+                  className="btn btn-primary m-1"
+                >
+                  Live Version
+                </button>
               </a>
-              <a href='https://github.com/RW2023/rw-images' target='_blank' rel='noopener noreferrer'>
-                <button type='button' aria-label='github' className="btn btn-secondary m-1">GitHub</button>
+              <a
+                href="https://github.com/RW2023/rw-images"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button
+                  type="button"
+                  aria-label="github"
+                  className="btn btn-secondary m-1"
+                >
+                  GitHub
+                </button>
               </a>
             </div>
           </div>
         </motion.div>
-        <motion.div
-          className="card mx-auto items-center border rounded bg-base-300"
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5 }}
-        >
-          <div className="card-title">
-            <SubHeading
-              title={`Card Title 2`}
-              iconClass="fas fa-project-diagram"
-            />
-          </div>
-          <div className="card-body">
-            <p>This is the body for Card 2</p>
-          </div>
-        </motion.div>
-        <motion.div
-          className="card mx-auto items-center border rounded bg-base-300"
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5 }}
-        >
-          <div className="card-title">
-            <SubHeading
-              title={`Card Title 3`}
-              iconClass="fas fa-project-diagram"
-            />
-          </div>
-          <div className="card-body">
-            <p>This is the body for Card 3</p>
-          </div>
-        </motion.div>
-        <motion.div
-          className="card mx-auto items-center border rounded bg-base-300"
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5 }}
-        >
-          <div className="card-title">
-            <SubHeading
-              title={`Card Title 4`}
-              iconClass="fas fa-project-diagram"
-            />
-          </div>
-          <div className="card-body">
-            <p>This is the body for Card 4</p>
-          </div>
-        </motion.div>
-        <motion.div
-          className="card mx-auto items-center border rounded bg-base-300"
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5 }}
-        >
-          <div className="card-title">
-            <SubHeading
-              title={`Card Title 5`}
-              iconClass="fas fa-project-diagram"
-            />
-          </div>
-          <div className="card-body">
-            <p>This is the body for Card 5</p>
-          </div>
-        </motion.div>
-        <motion.div
-          className="card mx-auto items-center border rounded bg-base-300"
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5 }}
-        >
-          <div className="card-title">
-            <SubHeading
-              title={`Card Title 6`}
-              iconClass="fas fa-project-diagram"
-            />
-          </div>
-          <div className="card-body">
-            <p>This is the body for Card 6</p>
-          </div>
-        </motion.div>
+
+        {/* Other Cards - Duplicated Style */}
+        {[...Array(5)].map((_, index) => (
+          <motion.div
+            key={`card-${index + 2}`}
+            className="card items-center border rounded-md bg-base-300 mx-5 flex flex-col"
+            initial={{ opacity: 0, x: index % 2 === 0 ? -100 : 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <div className="card-title p-4">
+              <SubHeading
+                title={`Card Title ${index + 2}`}
+                iconClass="fas fa-project-diagram"
+              />
+            </div>
+            <div className="card-body mx-auto border-y glass">
+              <p>This is the body for Card {index + 2}</p>
+            </div>
+          </motion.div>
+        ))}
       </div>
     </>
   );
