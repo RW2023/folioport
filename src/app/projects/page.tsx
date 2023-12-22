147fa610f16c8cddd85518e834a1ee6643ed5edd
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-22 06:58:46 -0500

    Update project page content and add flixer project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -14,10 +14,10 @@ const page: FC<Props> = (): JSX.Element => {
       <div className="flex flex-col items-center m-4 p-1 bg-base-100">
         <Heading title="projects" iconClass="fas fa-cogs" />
         <SubHeading
-          title="This is a subheading on the projects Page"
+          title="Live and Codebase"
           iconClass="fas fa-project-diagram"
         />
-        <p className="text-xl">This is paragraph font on the Projects Page</p>
+        <p className="text-xl">My Deployed projects</p>
       </div>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 mb-5">
         {/* First Card - Original Style */}
@@ -35,9 +35,7 @@ const page: FC<Props> = (): JSX.Element => {
           </div>
           <div className="card-body mx-auto border-y glass">
             <div className="card rounded border-1 border-gray-500 drop-shadow-md">
-              <Link
-              href="projects/photography"
-              >
+              <Link href="projects/photography">
                 <Image
                   src="/projects/Ryan-Wilson-Images.png"
                   alt="Ryan Wilson"
@@ -82,9 +80,67 @@ const page: FC<Props> = (): JSX.Element => {
             </div>
           </div>
         </motion.div>
+        {/* First Card - Original Style */}
+        <motion.div
+          className="card items-center border rounded-md bg-base-300 mx-5 flex flex-col"
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <div className="card-title p-4">
+            <SubHeading
+              title={`movie search app`}
+              iconClass="fas fa-images"
+            />
+          </div>
+          <div className="card-body mx-auto border-y glass">
+            <div className="card rounded border-1 border-gray-500 drop-shadow-md">
+              <Link href="projects/flixer">
+                <Image
+                  src="/projects/flixer.png"
+                  alt="Ryan Wilson"
+                  width={500}
+                  height={300}
+                />
+              </Link>
+            </div>
+            <div className="card-text">
+              <p>
+                This is a movie search app that I built using NextJS. It is
+                build with typescript and uses the IMDB API to fetch movie data.
+              </p>
+              <a
+                href="https://movies2023.vercel.app/movies"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button
+                  type="button"
+                  aria-label="live version"
+                  className="btn btn-primary m-1"
+                >
+                  Live Version
+                </button>
+              </a>
+              <a
+                href="https://github.com/RW2023/next-movies"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button
+                  type="button"
+                  aria-label="github"
+                  className="btn btn-secondary m-1"
+                >
+                  GitHub
+                </button>
+              </a>
+            </div>
+          </div>
+        </motion.div>
 
         {/* Other Cards - Duplicated Style */}
-        {[...Array(5)].map((_, index) => (
+        {[...Array(4)].map((_, index) => (
           <motion.div
             key={`card-${index + 2}`}
             className="card items-center border rounded-md bg-base-300 mx-5 flex flex-col"
