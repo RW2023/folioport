dd14d73171fe66625dfd34620bfcbe3a117bda9e
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-29 17:47:21 -0500

    Refactor ProjectComponent.tsx match original styling

--- src/components/ProjectComponent.tsx
+++ src/components/ProjectComponent.tsx
@@ -1,37 +1,75 @@
-// components/ProjectComponent.js
+// components/ProjectComponent.tsx
 import Image from 'next/image';
-import SubHeading from '@/components/ui/SubHeading'; 
+import Link from 'next/link';
+import SubHeading from '@/components/ui/SubHeading';
+import { motion } from 'framer-motion';
 
 export interface Project {
-    id: number;
-    title: string;
-    description: string;
-    image_url: string;
-    live_url: string;
-    github_url: string;
+  id: number;
+  title: string;
+  description: string;
+  image_url: string;
+  live_url: string;
+  github_url: string;
 }
 
 const ProjectComponent = ({ project }: { project: Project }) => {
-    return (
-        
-        <div className="project-card">
-            <SubHeading title={project.title} iconClass="fas fa-project-diagram" />{' '}
-            {/* Choose an icon that suits your project */}
-            <Image
+  return (
+    <motion.div
+      className="card items-center border rounded-md bg-base-300 mx-5 flex flex-col"
+      initial={{ opacity: 0, x: -100 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="card-title p-4">
+        <SubHeading title={project.title} iconClass="fas fa-project-diagram" />
+      </div>
+      <div className="card-body mx-auto border-y glass">
+        <div className="card rounded border-1 border-gray-500 drop-shadow-md">
+          <Link
+            href={`/projects/${project.title
+              .toLowerCase()
+              .split(' ')
+              .join('-')}`}
+          >
+            
+              <Image
                 src={project.image_url}
                 alt={project.title}
                 width={500}
                 height={300}
-            />
-            <p>{project.description}</p>
-            <a href={project.live_url} target="_blank" rel="noopener noreferrer">
-                Live Version
-            </a>
-            <a href={project.github_url} target="_blank" rel="noopener noreferrer">
-                GitHub
-            </a>
+              />
+            
+          </Link>
         </div>
-    );
+        <div className="card-text">
+          <p>{project.description}</p>
+          <a href={project.live_url} target="_blank" rel="noopener noreferrer">
+            <button
+              type="button"
+              aria-label="live version"
+              className="btn btn-primary m-1"
+            >
+              Live Version
+            </button>
+          </a>
+          <a
+            href={project.github_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button
+              type="button"
+              aria-label="github"
+              className="btn btn-secondary m-1"
+            >
+              GitHub
+            </button>
+          </a>
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default ProjectComponent;
