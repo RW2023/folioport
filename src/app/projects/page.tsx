9a36d8e7d03c50de1b95eedd1379f7f186f7afbc
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-29 19:05:30 -0500

    Refactor ProjectsPage component to include loading state

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,36 +1,47 @@
-'use client';
 // pages/projects.tsx
+'use client';
+
 import React, { useEffect, useState } from 'react';
-import { supabase } from '@/utils/supabaseClient'; // Update the path to your Supabase client
-import ProjectComponent from '@/components/ProjectComponent'; // Update the path to your ProjectComponent
-import { Project } from '@/types/project'; // Update the path to your types if necessary
+import { supabase } from '@/utils/supabaseClient';
+import ProjectComponent from '@/components/ProjectComponent';
+import { Project } from '@/types/project';
+import LoadingComponent from '@/components/ui/Loading';
 
 const ProjectsPage: React.FC = () => {
-    const [projects, setProjects] = useState<Project[]>([]);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-    useEffect(() => {
-        const fetchProjects = async () => {
-            const { data: projects, error } = await supabase
-                .from('projects')
-                .select('*');
+  useEffect(() => {
+    const fetchProjects = async () => {
+      setIsLoading(true);
+      const { data: projects, error } = await supabase
+        .from('projects')
+        .select('*');
 
-            if (!error && projects) {
-                setProjects(projects as Project[]);
-            } else {
-                console.error('Error fetching projects:', error);
-            }
-        };
+      if (error) {
+        console.error('Error fetching projects:', error);
+      } else if (projects) {
+        setProjects(projects);
+      }
+      setIsLoading(false);
+    };
 
-        fetchProjects();
-    }, []);
+    fetchProjects();
+  }, []);
 
-    return (
-        <div className='min-h-screen'>
-            {projects.map((project) => (
-                <ProjectComponent key={project.id} project={project} />
-            ))}
-        </div>
-    );
+  if (isLoading) {
+    return <LoadingComponent />;
+  }
+
+  return (
+    <div className="container mx-auto p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {projects.map((project) => (
+          <ProjectComponent key={project.id} project={project} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProjectsPage;
