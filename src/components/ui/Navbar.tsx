35570773ed3374fe5462666e8f93a67bb6433ac4
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-04 17:23:04 -0500

    Update Navbar link icon

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -133,7 +133,7 @@ const Navbar = () => {
               onClick={closeNavbar}
               className="block mt-4 lg:inline-block lg:mt-0 text-strokeLight text-xl  hover:underline mr-4 cursor-pointer"
             >
-              <i className="fas fa-cocktail mr-2"></i>Bar{' '}
+              <i className="fas fa-cogs mr-2"></i>Projects{' '}
             </span>
           </Link>
           <Link href="/about">
